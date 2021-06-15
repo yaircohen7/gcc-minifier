c31957aa59c9b7b9f8198211edc8066be8b66492
Yair Cohen <yaircohen7@gmail.com>
2021-06-15 22:09:07 +0300

    Update packages

--- file.js
+++
@@ -1,9 +0,0 @@
-const handleFileUpload = async (req,res) => {
-
-    let token = await randomToken();
-    let files = await getFiles(req);
-    let [source] = await saveFiles(token,files);
-    let scriptSummary = await runScript(source);
-
-    handleFileUploadResponse(res,scriptSummary);
-};
